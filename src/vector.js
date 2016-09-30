7932b36092eb583aad416aca7910547f13c123cc
jkvim <jkvim@outlook.com>
2016-09-30 19:41:58 +0800

    vector: fix lint error

--- src/vector.js
+++ src/vector.js
@@ -2,21 +2,25 @@ import { rand } from './util';
 
 export default class Vector {
   constructor(array = []) {
-    this._elems = Array.from(array);
+    if (array instanceof Vector) {
+      this.elems = Array.from(array.elems);
+    } else {
+      this.elems = Array.from(array);
+    }
   }
 
-  _swap(a, b) {
-    var temp = this._elems[a];
-    this._elems[a] = this._elems[b];
-    this._elems[b] = temp;
+  swap(a, b) {
+    const temp = this.elems[a];
+    this.elems[a] = this.elems[b];
+    this.elems[b] = temp;
   }
 
   bubble(lo, hi) {
-    var sorted = true;
+    let sorted = true;
     while (++lo < hi) {
-      if (this._elems[lo-1] > this._elems[lo]) {
+      if (this.elems[lo - 1] > this.elems[lo]) {
         sorted = false;
-        this._swap(lo - 1, lo);
+        this.swap(lo - 1, lo);
       }
     }
     return sorted;
@@ -26,10 +30,10 @@ export default class Vector {
     while (!this.bubble(lo, hi--));
   }
 
-  _max(lo, hi) {
-    var max = lo;
-    for (var i = lo; i < hi; i++) {
-      if (this._elems[max] < this._elems[i]) {
+  max(lo, hi) {
+    let max = lo;
+    for (let i = lo; i < hi; i++) {
+      if (this.elems[max] < this.elems[i]) {
         max = i;
       }
     }
@@ -37,31 +41,30 @@ export default class Vector {
   }
 
   selectionSort(lo, hi) {
-    for (var i = hi; 1 < i; i--) {
-      var max = this._max(lo, i);
-      this._swap(max, i - 1);
+    for (let i = hi; i > 1; i--) {
+      this.swap(this.max(lo, i), i - 1);
     }
   }
 
   insertionSort(lo, hi) {
-    for (var i = lo + 1; i < hi; i++) {
-      var e = this._elems[i];
-      var p = this.binSearch(e, lo, i);
-      this.insert(p+1, e);
+    for (let i = lo + 1; i < hi; i++) {
+      const e = this.elems[i];
+      const p = this.binSearch(e, lo, i);
+      this.insert(p + 1, e);
     }
   }
 
   merge(lo, mi, hi) {
-    var la = mi - lo;
-    var lb = hi - mi;
-    var tempA = this._elems.slice(lo, mi);
-    var tempB = this._elems.slice(mi, hi);
-    for (var i = lo, j = 0, k = 0; (j < la) || (k < lb);) {
+    const la = mi - lo;
+    const lb = hi - mi;
+    const tempA = this.elems.slice(lo, mi);
+    const tempB = this.elems.slice(mi, hi);
+    for (let i = lo, j = 0, k = 0; (j < la) || (k < lb);) {
       if ((j < la) && (!(k < lb) || (tempA[j] <= tempB[k]))) {
-        this._elems[i++] = tempA[j++];
+        this.elems[i++] = tempA[j++];
       }
       if ((k < lb) && (!(j < la) || (tempB[k] < tempA[j]))) {
-        this._elems[i++] = tempB[k++];
+        this.elems[i++] = tempB[k++];
       }
     }
   }
@@ -70,7 +73,7 @@ export default class Vector {
     if (hi - lo < 2) {
       return;
     }
-    var mi = Math.round((lo + hi) / 2);
+    const mi = Math.round((lo + hi) / 2);
     this.mergeSort(lo, mi);
     this.mergeSort(mi, hi);
     this.merge(lo, mi, hi);
@@ -90,7 +93,7 @@ export default class Vector {
 
   // 只读接口
   size() {
-    return this._elems.length;
+    return this.elems.length;
   }
 
   empty() {
@@ -98,9 +101,9 @@ export default class Vector {
   }
 
   disordered() {
-    var n = 0;
-    for (var i = 1; i < this._elems.length; i++) {
-      if (this._elems[i - 1] > this._elems[i]) {
+    let n = 0;
+    for (let i = 1; i < this.elems.length; i++) {
+      if (this.elems[i - 1] > this.elems[i]) {
         n++;
       }
     }
@@ -109,50 +112,51 @@ export default class Vector {
 
   // 无序查找
   find(e) {
-    return this._find(e, 0, this.size());
+    return this.findInRange(e, 0, this.size());
   }
 
-  _find(e, lo, hi) {
-    while ((lo < hi--) && (e !== this._elems[hi])); // 从后向前查找
+  findInRange(e, lo, hi) {
+    while ((lo < hi--) && (e !== this.elems[hi])); // 从后向前查找
     return hi;  // 失败返回 lo - 1
   }
 
   // 有序查找
   search(e) {
-    return 0 <= this.size() ? this.binSearch(e, 0, this.size()) : -1; 
+    return this.size() >= 0 ? this.binSearch(e, 0, this.size()) : -1;
   }
 
   binSearch(e, lo, hi) {
     while (lo < hi) {
-      var mi = (lo + hi) >> 1;
-      (e < this._elems[mi]) ? hi = mi : lo = mi + 1;
+      const mi = Math.floor((lo + hi) / 2);
+      (e < this.elems[mi]) ? hi = mi : lo = mi + 1;
     }
     return --lo;  // 返回不大于 e 的最大秩，找不到返回-1
   }
 
   // 可写访问接口
   get(r) {
-    return this._elems[r];
+    return this.elems[r];
   }
 
   set(r, e) {
-    return this._elems[r] = e;
+    this.elems[r] = e;
+    return this.elems[r];
   }
 
   remove(r) {
-    var e = this._elems[r];
-    this._remove(r, r + 1);
+    const e = this.elems[r];
+    this.removeInRange(r, r + 1);
     return e;
   }
 
-  _remove(lo, hi) {
+  removeInRange(lo, hi) {
     if (lo === hi) {
       return 0;
     }
     while (hi < this.size()) {
-      this._elems[lo++] = this._elems[hi++];
+      this.elems[lo++] = this.elems[hi++];
     }
-    this._elems.splice(lo);
+    this.elems.splice(lo);
     return hi - lo;
   }
 
@@ -161,18 +165,18 @@ export default class Vector {
       e = r;
       r = this.size();
     }
-    for (var i = this.size(); i > r; i--) {
-      this._elems[i] = this._elems[i - 1];
+    for (let i = this.size(); i > r; i--) {
+      this.elems[i] = this.elems[i - 1];
     }
-    this._elems[r] = e;
+    this.elems[r] = e;
     return r;
   }
 
   sort() {
-    return this._sort(0, this.size());
+    return Vector.sortInRange(0, this.size());
   }
 
-  _sort(lo, hi) {
+  static sortInRange(lo, hi) {
     switch (rand() % 5) {
       case 1: bubbleSort(lo, hi);
         break;
@@ -188,31 +192,31 @@ export default class Vector {
   }
 
   unsort() {
-    this._unsort(0, this.size());
+    this.unsortInRange(0, this.size());
   }
 
-  _unsort(lo, hi) {
-    for (var i = hi; i > 0; i--) {
-      this._swap(i - 1, rand() % i);
+  unsortInRange(lo, hi) {
+    for (let i = hi; i > 0; i--) {
+      this.swap(i - 1, rand() % i);
     }
   }
 
   deduplicate() {
-    var oldSize = this.size();
-    var i = 1;
+    const oldSize = this.size();
+    let i = 1;
     while (i < this.size()) {
-      (this._find(this._elems[i], 0, i) < 0) ?
-      i++: this.remove(i);
+      (this.findInRange(this.elems[i], 0, i) < 0) ?
+      i++ : this.remove(i);
     }
     return oldSize - this.size();
   }
 
   uniquify() {
-    var i = 0;
-    var j = 0;
+    let i = 0;
+    let j = 0;
     while (j++ < this.size()) {
-      if (this._elems[j] !== this._elems[i]) {
-        this._elems[++i] = this._elems[j];
+      if (this.elems[j] !== this.elems[i]) {
+        this.elems[++i] = this.elems[j];
       }
     }
     ++i;
@@ -220,10 +224,10 @@ export default class Vector {
   }
 
   traverse(visit) {
-    for (var i = 0; i < this.size(); i++) {
-      visit(this._elems[i]);
+    for (let i = 0; i < this.size(); i++) {
+      visit(this.elems[i]);
     }
   }
-};
+}
 
 module.exports = Vector;
--- test/test_vector.js
+++ test/test_vector.js
@@ -1,44 +1,44 @@
 import { expect } from 'chai';
 import Vector from '../src/vector';
 
-describe('Vector', function () {
-  describe('getter', function () {
-    it('should get by rank', function () {
-      var vector = new Vector([0, 1, 2]);
-      var value = vector.get(2);
+describe('Vector', () => {
+  describe('getter', () => {
+    it('should get by rank', () => {
+      const vector = new Vector([0, 1, 2]);
+      const value = vector.get(2);
       expect(value).to.equal(2);
     });
   });
 
-  describe('setter', function () {
-    it('should set by rank', function () {
-      var vector = new Vector([]);
+  describe('setter', () => {
+    it('should set by rank', () => {
+      const vector = new Vector([]);
       vector.set(0, 2);
       expect(vector.get(0)).to.equal(2);
     });
   });
 
-  describe('bubbleSort', function () {
-    it('should become sorted', function () {
-      var vector = new Vector([10, 2, 4, 19, 7]);
+  describe('bubbleSort', () => {
+    it('should become sorted', () => {
+      const vector = new Vector([10, 2, 4, 19, 7]);
       vector.bubbleSort(0, vector.size());
       expect(vector.disordered()).to.equal(0);
     });
   });
 
-  describe('selectionSort', function () {
-    it('should become sorted', function () {
-      var vector = new Vector([8, 5, 7, 1]);
+  describe('selectionSort', () => {
+    it('should become sorted', () => {
+      const vector = new Vector([8, 5, 7, 1]);
       vector.selectionSort(0, vector.size());
       expect(vector.disordered()).to.equal(0);
     });
   });
 
-  describe('mergeSort', function () {
-    it('should become sorted', function () {
-      var vectorA = new Vector([9, 8, 3, 0]);
-      var vectorB = new Vector([8, 1, 0]);
-      var vectorC = new Vector([0]);
+  describe('mergeSort', () => {
+    it('should become sorted', () => {
+      const vectorA = new Vector([9, 8, 3, 0]);
+      const vectorB = new Vector([8, 1, 0]);
+      const vectorC = new Vector([0]);
       vectorA.mergeSort(0, vectorA.size());
       vectorB.mergeSort(0, vectorB.size());
       vectorC.mergeSort(0, vectorC.size());
@@ -48,100 +48,100 @@ describe('Vector', function () {
     });
   });
 
-  describe('empty', function () {
-    it('should get true when empty', function () {
-      var vector = new Vector([]);
+  describe('empty', () => {
+    it('should get true when empty', () => {
+      const vector = new Vector([]);
       expect(vector.empty()).to.equal(true);
     });
 
-    it('should get false when not empty', function () {
-      var vector = new Vector([1, 2, 3]);
+    it('should get false when not empty', () => {
+      const vector = new Vector([1, 2, 3]);
       expect(vector.empty()).to.equal(false);
     });
   });
 
-  describe('disordered', function () {
-    it('should get 2 with 2 pair adjacent reverse element', function () {
-      var vector = new Vector([1, 0, 5, 4]);
+  describe('disordered', () => {
+    it('should get 2 with 2 pair adjacent reverse element', () => {
+      const vector = new Vector([1, 0, 5, 4]);
       expect(vector.disordered()).to.equal(2);
     });
   });
 
-  describe('find', function () {
-    it('should return -1 when not exist', function () {
-      var vector = new Vector([1, 2, 3]);
+  describe('find', () => {
+    it('should return -1 when not exist', () => {
+      const vector = new Vector([1, 2, 3]);
       expect(vector.find(0)).to.equal(-1);
     });
 
-    it('should return lastest rank when found', function () {
-      var vector = new Vector([1, 3, 5, 5, 7]);
+    it('should return lastest rank when found', () => {
+      const vector = new Vector([1, 3, 5, 5, 7]);
       expect(vector.find(5)).to.equal(3);
     });
   });
 
-  describe('search', function () {
-    it('should return -1 not found and small than a[0]', function () {
-      var vector = new Vector([9, 10 ,12]);
+  describe('search', () => {
+    it('should return -1 not found and small than a[0]', () => {
+      const vector = new Vector([9, 10,12]);
       expect(vector.search(8)).to.equal(-1);
     });
 
-    it('should return lastest index when found', function () {
-      var vector = new Vector([9, 10, 10, 11, 12]);
+    it('should return lastest index when found', () => {
+      const vector = new Vector([9, 10, 10, 11, 12]);
       expect(vector.search(10)).to.equal(2);
     });
   });
 
-  describe('remove', function () {
-    it('should remove element by index', function () {
-      var vector = new Vector([1, 3, 5]);
+  describe('remove', () => {
+    it('should remove element by index', () => {
+      const vector = new Vector([1, 3, 5]);
       vector.remove(0);
       expect(vector.find(1)).to.equal(-1);
       expect(vector.size()).to.equal(2);
     });
   });
 
-  describe('insert', function () {
-    it('should insert element with index', function () {
-      var vector = new Vector([1, 3, 5]);
+  describe('insert', () => {
+    it('should insert element with index', () => {
+      const vector = new Vector([1, 3, 5]);
       vector.insert(1, 2);
       expect(vector.find(2)).to.equal(1);
       expect(vector.size()).to.equal(4);
     });
 
-    it('should insert at last without index', function () {
-      var vector = new Vector([1]);
+    it('should insert at last without index', () => {
+      const vector = new Vector([1]);
       vector.insert(0);
       expect(vector.get(1)).to.equal(0);
     });
   });
 
-  describe('unsort', function () {
-    it('should make the sorted to unsort', function () {
-      var vector = new Vector([1, 4, 9, 10]);
+  describe('unsort', () => {
+    it('should make the sorted to unsort', () => {
+      const vector = new Vector([1, 4, 9, 10]);
       vector.unsort();
       expect(vector.disordered()).to.be.above(0);
     });
   });
 
-  describe('deduplicate', function () {
-    it('should remove equal element', function () {
-      var vector = new Vector([1, 2, 3, 3, 3]);
+  describe('deduplicate', () => {
+    it('should remove equal element', () => {
+      const vector = new Vector([1, 2, 3, 3, 3]);
       expect(vector.deduplicate()).to.equal(2);
     });
   });
 
-  describe('uniquify', function () {
-    it('should remove equal element', function () {
-      var vector = new Vector([3, 3, 3]);
+  describe('uniquify', () => {
+    it('should remove equal element', () => {
+      const vector = new Vector([3, 3, 3]);
       expect(vector.uniquify()).to.equal(2);
     });
   });
 
-  describe('traverse', function () {
-    it('should pass element to callback in sequence', function () {
-      var vector = new Vector([1, 2, 3]);
-      var temp = [];
-      vector.traverse(function (e) {
+  describe('traverse', () => {
+    it('should pass element to callback in sequence', () => {
+      const vector = new Vector([1, 2, 3]);
+      const temp = [];
+      vector.traverse((e) => {
         temp.push(e);
       });
       expect(temp).to.eql([1, 2, 3]);
